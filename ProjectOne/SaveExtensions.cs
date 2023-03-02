c30897a9f893c28dfeb25ee402d209d53b1cfb19
Wojtek Pugowski <Wojtek@BootCamp>
2023-03-02 11:18:47 +0100

    Added camelCase to serialization's properties.

---
+++ ProjectOne/SaveExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace ProjectOne
+{
+    public static class SaveExtensions
+    {
+
+        public static void SaveToFile(Repository repo, string fileName)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            string serializatedRepo = JsonSerializer.Serialize(repo, options);
+            
+            using (var fileStream = new FileStream(fileName, FileMode.Create))
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(serializatedRepo);
+                fileStream.Write(bytes);
+            }
+        }     }
+}
